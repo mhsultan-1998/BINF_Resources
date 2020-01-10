0ce40fd0d1f165a3e01d2b067218f20ef7038199
julianstanley <stanley.ju@husky.neu.edu>
2020-01-10 13:31:15 -0500

    Added addition correct testout answer for fasta sliding window

--- BINF6309/Module01/Assignment_Tests/test_sliding_window_fasta.sh
+++ BINF6309/Module01/Assignment_Tests/test_sliding_window_fasta.sh
@@ -12,7 +12,8 @@ fi
 
 output_example=$(python3 sliding_window_fasta.py 20 dengue.fasta 2>/dev/null)
 output_correct=$(cat sliding_window_fasta_testout)
-if [ "$output_example" == "$output_correct" ]; then
+output_correct_also=$(cat sliding_window_fasta_testout2)
+if [ "$output_example" == "$output_correct" ] || [ "$output_example" == "$output_correct_also" ]; then
     echo -e "${GREEN}PASSED:${NC} Dengue virus with 20-mers"
     else
     echo -e "${RED}FAILED:${NC} Dengue virus with 20-mers"
